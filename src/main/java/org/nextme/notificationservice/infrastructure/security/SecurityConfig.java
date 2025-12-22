5a55d0a5a6f242ac12e6c49fe173c62bc4b103b1
kms <minsu00222@gmail.com>
2025-12-22 15:54:37 +0900

    쿠버네티스 배포 환경으로 변경 v3

--- src/main/java/org/nextme/notificationservice/infrastructure/security/SecurityConfig.java
+++ src/main/java/org/nextme/notificationservice/infrastructure/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +14,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                // 3. 요청 권한 설정
                 .authorizeHttpRequests(auth -> auth
+                        // Actuator 경로는 인증 없이 접근 가능하도록 명시적 허용
+                        .requestMatchers("/actuator/**").permitAll()
+                        // 그 외 모든 요청도 현재는 허용 (테스트 단계)
                         .anyRequest().permitAll()
                 );
+
         return http.build();
     }
 }
